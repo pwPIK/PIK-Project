4576383706d2cd34207dd705c61e748dd954b229
Galfaroth7 <misiekostap@wp.pl>
2016-04-05 15:13:09 +0200

    Added ninja message

--- src/main/java/application/App.java
+++ src/main/java/application/App.java
@@ -7,6 +7,7 @@ public class App {
     public static void main(String[] args) {
 
         System.out.println("Testowa wiadomosc");
+        System.out.println("Ninja Review nie działa");
     }
     
     public boolean isEven(int i) {
