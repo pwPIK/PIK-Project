48d5e676276b15bfa3542c1c21bb9ca3a1e221f2
Galfaroth7 <misiekostap@wp.pl>
2016-04-05 15:07:05 +0200

    Added test message

--- src/main/java/application/App.java
+++ src/main/java/application/App.java
@@ -5,6 +5,8 @@ public class App {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+
+        System.out.println("Testowa wiadomosc");
     }
     
     public boolean isEven(int i) {
