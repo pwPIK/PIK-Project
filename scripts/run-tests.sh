673e8a3f4dc309e0186b6babbe75c2bcf3151757
SecurityManPL <s.e.c.u.m.a.n.p.l@gmail.com>
2016-04-14 17:14:49 +0200

    Some integration test added.

--- scripts/run-tests.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -ev						
-# flaga -e powoduje zakończenie wykonywania skryptu jak tylko któraś instrukcja zwróci niezerowy kod wyjściowy
-# flaga -v powoduje wypisywanie lini skryptu przed jej wykonaniem, co pomaga w znajdowaniu miejsca błędu
-bundle exec rake:units
-if [ "${TRAVIS_PULL_REQUEST}" = "false" ]; then
-	bundle exec rake test:integration
-fi
---
+++ src/integrationTest/integrationTest.java
@@ -0,0 +1,17 @@
+package application;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Bartlomiej Kunikowski
+ */
+public class IntegrationTest {
+
+    @Test
+    public void SomeTest() {
+        assertTrue(false);
+    }
+    
+}
