1011567731f8e977e0905e353e8606b83c92261b
SecurityManPL <s.e.c.u.m.a.n.p.l@gmail.com>
2016-04-14 21:26:33 +0200

    Next try to build integration tests. Previous try was total failure.

---
+++ src/integrationTest/application/integrationTest.java
@@ -0,0 +1,21 @@
+package application;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+*
+* @author Bartlomiej Kunikowski
+*/
+public class integrationTest {
+    
+    public integrationTest() {
+    }
+
+    @Test
+    public void fooTest() {
+        App app = new App();
+        assertTrue(true);
+    }
+    
+}
